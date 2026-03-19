e5df35af7c21841ac5635c4304a1fd43990d443c
Bin Sula <balixsulait@gmail.com>
2026-03-19 15:34:40 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Testing the Stack API:");
+        Stack<Integer> stack = new Stack<>();
+
+        System.out.println("Stack isEmpty? " + stack.isEmpty());
+
+        System.out.println("Pushing 10, 20, 30...");
+        stack.push(10);
+        stack.push(20);
+        stack.push(30);
+
+        System.out.println("Stack size: " + stack.size());
+        System.out.println("Top element (peek): " + stack.peek());
+
+        System.out.println("Popping elements:");
+        while (!stack.isEmpty()) {
+            System.out.println("Popped: " + stack.pop());
+        }
+
+        System.out.println("Stack isEmpty? " + stack.isEmpty());
+    }
+}
---
+++ Stack.java
@@ -0,0 +1,74 @@
+import java.util.EmptyStackException;
+
+public class Stack<T> {
+    private Node<T> top;
+    private int size;
+
+    private static class Node<T> {
+        T data;
+        Node<T> next;
+
+        Node(T data) {
+            this.data = data;
+        }
+    }
+
+    public Stack() {
+        this.top = null;
+        this.size = 0;
+    }
+
+    /**
+     * Pushes an item onto the top of this stack.
+     * @param item the item to be pushed onto this stack.
+     */
+    public void push(T item) {
+        Node<T> newNode = new Node<>(item);
+        newNode.next = top;
+        top = newNode;
+        size++;
+    }
+
+    /**
+     * Removes the object at the top of this stack and returns that object as the value of this function.
+     * @return The object at the top of this stack.
+     * @throws EmptyStackException if this stack is empty.
+     */
+    public T pop() {
+        if (isEmpty()) {
+            throw new EmptyStackException();
+        }
+        T data = top.data;
+        top = top.next;
+        size--;
+        return data;
+    }
+
+    /**
+     * Looks at the object at the top of this stack without removing it from the stack.
+     * @return the object at the top of this stack.
+     * @throws EmptyStackException if this stack is empty.
+     */
+    public T peek() {
+        if (isEmpty()) {
+            throw new EmptyStackException();
+        }
+        return top.data;
+    }
+
+    /**
+     * Tests if this stack is empty.
+     * @return true if and only if this stack contains no items; false otherwise.
+     */
+    public boolean isEmpty() {
+        return top == null;
+    }
+
+    /**
+     * Returns the number of elements in this stack.
+     * @return the number of elements in this stack.
+     */
+    public int size() {
+        return size;
+    }
+}
